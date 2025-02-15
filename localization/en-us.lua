6dc43fd5168a09a228fb28ef9a2da74724a367df
astrapboy <astrap.theboy@gmail.com>
2025-02-16 09:59:36 +1000

    Changed to quotation marks because I can

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -34,122 +34,122 @@ return {
         Joker={
             --- Temporary Joker! Used for testing code and stuff
             j_mstg_allan = {
-                name = 'Allan please add details',
+                name = "Allan please add details",
                 text = {
-                    'Allan please add details'
+                    "Allan please add details"
                 }
             },
             --- Common
             j_mstg_diminishingreturns = {
-                name = 'Diminishing Returns',
+                name = "Diminishing Returns",
                 text = {
-                    '{X:mult,C:white} X#1# {} Mult minus',
-                    '{X:mult,C:white} X#2# {} Mult for each',
-                    'card in played hand',
+                    "{X:mult,C:white} X#1# {} Mult minus",
+                    "{X:mult,C:white} X#2# {} Mult for each",
+                    "card in played hand",
                 }
             },
             j_mstg_peekingjoker = {
-                name = 'Peeking Joker',
+                name = "Peeking Joker",
                 text = {
-                    '{C:mult}+#1#{} Mult if chips',
-                    'scored are at least',
-                    '{C:attention}#2#%{} of required chips',
+                    "{C:mult}+#1#{} Mult if chips",
+                    "scored are at least",
+                    "{C:attention}#2#%{} of required chips",
                 }
             },
             j_mstg_ninjajoker = {
-                name = 'Ninja Joker',
+                name = "Ninja Joker",
                 text = {
-                    '{C:chips}+#1#{} Chips if chips',
-                    'scored are less than',
-                    '{C:attention}#2#%{} of required chips',
+                    "{C:chips}+#1#{} Chips if chips",
+                    "scored are less than",
+                    "{C:attention}#2#%{} of required chips",
                 }
             },
             j_mstg_timesheet = {
-                name = 'Timesheet',
+                name = "Timesheet",
                 text = {
-                    'Earn {C:money}$#1#{} every time',
-                    'a playing card is {C:attention}retriggered{}',
+                    "Earn {C:money}$#1#{} every time",
+                    "a playing card is {C:attention}retriggered{}",
                 }
             },
             j_mstg_uptoeleven = {
-                name = 'Up To 11',
+                name = "Up To 11",
                 text = {
-                    '{C:attention}10{}s and {C:attention}Aces{}',
-                    'are considered',
-                    '{C:attention}face{} cards'
+                    "{C:attention}10{}s and {C:attention}Aces{}",
+                    "are considered",
+                    "{C:attention}face{} cards"
                 }
             },
             --- Uncommon
             j_mstg_boulder= {
-                name = 'Boulder',
+                name = "Boulder",
                 text = {
-                    '{C:attention}+#1#{} free {C:green}Reroll{} every',
-                    'time you leave a shop',
-                    'without {C:attention}rerolling{}',
+                    "{C:attention}+#1#{} free {C:green}Reroll{} every",
+                    "time you leave a shop",
+                    "without {C:attention}rerolling{}",
                     "{C:inactive}(Max of {C:green}#2#{C:inactive} Rerolls)",
                     "{C:inactive}(Currently {C:green}#3#{C:inactive} Rerolls)",
                 }
             },
             j_mstg_comedian= {
-                name = 'Comedian',
+                name = "Comedian",
                 text = {                   
                     "Retrigger",
                     "each played",
                     "{C:attention}#1#{}, {C:attention}#2#{}, {C:attention}#3#{}, or {C:attention}#4#{}",                }
             },
             j_mstg_handyman = {
-                name = 'Handyman',
+                name = "Handyman",
                 text = {
-                    '{X:mult,C:white} X#1# {} Mult if',
-                    '{C:attention}played hand{} matches',
-                    'example in the menu',
-                    '{C:inactive}(Includes non-scoring cards){}'
+                    "{X:mult,C:white} X#1# {} Mult if",
+                    "{C:attention}played hand{} matches",
+                    "example in the menu",
+                    "{C:inactive}(Includes non-scoring cards){}"
                 }
             },
             j_mstg_bananafactory = {
-                name = 'Banana Factory',
+                name = "Banana Factory",
                 text = {
-                    'Guarantees a {C:attention}Gros Michel{}',
-                    'or {C:attention}Cavendish{} in the',
-                    'next shop',
-                    '{C:inactive}(If not owned){}'
+                    "Guarantees a {C:attention}Gros Michel{}",
+                    "or {C:attention}Cavendish{} in the",
+                    "next shop",
+                    "{C:inactive}(If not owned){}"
                 }
             },
             --- Rare
             j_mstg_outcast = {
-                name = 'Outcast',
+                name = "Outcast",
                 text = {
-                    '{X:mult,C:white} X#1# {} Mult for every',
-                    'hand played that includes a',
-                    '{C:attention}non-scoring{} card, resets',
-                    'if all cards score',
-                    '{C:inactive}(Currently {X:mult,C:white} X#2# {C:inactive} Mult)',
+                    "{X:mult,C:white} X#1# {} Mult for every",
+                    "hand played that includes a",
+                    "{C:attention}non-scoring{} card, resets",
+                    "if all cards score",
+                    "{C:inactive}(Currently {X:mult,C:white} X#2# {C:inactive} Mult)",
                 }
             },
             j_mstg_powerofthree= {
-                name = 'Power of Three',
+                name = "Power of Three",
                 text = {
-                    '{X:mult,C:white} X#1# {} Mult for each',
-                    'scored hand this run',
-                    'that contains a {C:attention}#2#{}',
+                    "{X:mult,C:white} X#1# {} Mult for each",
+                    "scored hand this run",
+                    "that contains a {C:attention}#2#{}",
                     "{C:inactive}(Currently {X:mult,C:white} X#3# {C:inactive} Mult)",
                 }
             },
             j_mstg_plasmajoker= {
-                name = 'Plasma Joker',
+                name = "Plasma Joker",
                 text = {
-                    'Up to {X:mult,C:white} X#1# {} Mult',
-                    'depending on how balanced',
-                    '{C:chips}Chips{} and {C:mult}Mult{} are'
+                    "Up to {X:mult,C:white} X#1# {} Mult",
+                    "depending on how balanced",
+                    "{C:chips}Chips{} and {C:mult}Mult{} are"
                 }
             },
             j_mstg_weighteddice= {
-                name = 'Weighted Dice',
+                name = "Weighted Dice",
                 text = {
-                    'After Blind is selected,',
-                    'sell this Joker to {C:attention}guarantee',
-                    '{C:green}all probabilities{} until the',
-                    'end of the round',
+                    "After Blind is selected,",
+                    "sell this Joker to {C:attention}guarantee",
+                    "{C:green}all probabilities{} until the",
+                    "end of the round",
                 }
             },
         },
