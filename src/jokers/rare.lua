87d895c13169c1baee4567e812ed2ad8ba341f4f
astrapboy <astrap.theboy@gmail.com>
2025-02-16 19:00:39 +1000

    Reverted the new Joker to how it was before
    For SMODS compatibility with older versions

--- src/jokers/rare.lua
+++ src/jokers/rare.lua
@@ -93,6 +93,7 @@ create_joker({
     end
 })
 
+-- Weighted Dice
 create_joker({
     key = "weighteddice",
     credits = {
@@ -121,32 +122,44 @@ create_joker({
 })
 
 -- Conscription
-create_joker({
+create_joker {
     key = "conscription",
     credits = {
         idea = "astrapboy",
-        code = "astrapboy & Khaki"
+        code = "astrapboy and Khaki"
     },
     blueprint = false,
-    rarity = "R",
-    cost = 9,
+    rarity = "U",
+    cost = 7,
     calculate = function(self, card, context)
-        local eval = function() return (G.GAME.current_round.hands_played == 0 and not context.blueprint) and not G.RESET_JIGGLES end
-        juice_card_until(card, eval, true)
-
         if context.after and G.GAME.current_round.hands_played == 0 and not context.blueprint then
+            local safes = {}
+            for s_idx = 1, #context.scoring_hand do
+                local c = context.scoring_hand[s_idx]
+                table.insert(safes, c:get_id())
+            end -- full_hand
             for c_idx = 1, #context.full_hand do
                 local c = context.full_hand[c_idx]
-                if not SMODS.in_scoring(c, context.scoring_hand) then
+
+                local found = false
+
+                for _, id in pairs(safes) do
+                    if id == c:get_id() then
+                        found = true
+                        break
+                    end
+                end
+
+                if not found then
                     add_event(function()
                         if G.jokers then
                             c:juice_up(0.8, 0.8)
                             c:start_dissolve({ HEX("63f06b") }, nil, 1.6)
                             return true
                         end
-                    end, nil, nil, "immediate")
+                    end, nil, nil)
                 end
             end
         end
     end
-})
+}
