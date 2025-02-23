c732c2687399a6b85fa0736a48824d92f7fae061
astrapboy <astrap.theboy@gmail.com>
2025-02-23 13:03:03 +1000

    Removed Lovely patches
    Now we mostly use Hooks to avoid interfering too badly with other mods

--- src/lib/deps/game_hooks.lua
+++ src/lib/deps/game_hooks.lua
@@ -1,10 +1,33 @@
-local igo = Game.init_game_object
+-- Game Functions
+local Game_igoRef = Game.init_game_object
 Game.init_game_object = function(self)
-	local ref = igo(self)
+	local ref = Game_igoRef(self)
 	ref.probabilities.mstg_base_normal = 1
 	ref.mstg = {
 		unique_jokers = {},
 		first_joker = nil
 	}
 	return ref
+end
+
+-- Card Functions
+local Card_isfaceRef = Card.is_face
+Card.is_face = function(self, from_boss)
+	Card_isfaceRef(self, from_boss)
+	if next(SMODS.find_card("j_mstg_uptoeleven")) and id >= 10 then
+		return true
+	end
+end
+
+-- CardArea Functions
+local CardArea_emplaceRef = CardArea.emplace
+CardArea.emplace = function(self, card, location, stay_flipped)
+	CardArea_emplaceRef(self, card, location, stay_flipped)
+	if self == G.jokers then
+		local k = card.config.center.key
+		if G.GAME.mstg.unique_jokers[k] == nil then
+			table.insert(G.GAME.mstg.unique_jokers, k)
+			G.GAME.mstg.unique_jokers[k] = true
+		end
+	end
 end
