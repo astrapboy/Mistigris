a694b6b5cc2c083b1a615662a6ee662fcbaba5ed
astrapboy <astrap.theboy@gmail.com>
2025-03-02 09:24:09 +1000

    Invite letter further fixes

--- src/items/joker/rare/invitation_letter.lua
+++ src/items/joker/rare/invitation_letter.lua
@@ -20,7 +20,7 @@ local j = {
 				suit_count[v.base.suit] = suit_count[v.base.suit] + 1
 			end
 
-			local max_suit = "Spades"
+			local max_suit = pseudorandom_element(SMODS.Suits, pseudoseed("invitation_letter_create")).key
 			local max_count = 0
 			for k, v in pairs(suit_count) do
 				if v > max_count then
@@ -31,15 +31,20 @@ local j = {
 
 			local copying_card, copying_card_new = mistiutils.get_random_card_in_deck_of_suit(max_suit)
 			local c = copy_card(copying_card, nil, nil, copying_card_new)
-			mistiutils.add_event(function()
-				G.hand:emplace(c)
-				c:start_materialize()
-				G.GAME.blind:debuff_card(c)
-				G.hand:sort()
-				if context.blueprint_card then context.blueprint_card:juice_up() else card:juice_up() end
-				return true
-			end)
+			c:add_to_deck()
+			G.deck.config.card_limit = G.deck.config.card_limit + 1
+			table.insert(G.playing_cards, c)
+			G.hand:emplace(c)
+			c:start_materialize()
+			G.GAME.blind:debuff_card(c)
+			G.hand:sort()
 			playing_card_joker_effects({ c })
+
+			return {
+				message = localize('k_copied_ex'),
+				colour = G.C.CHIPS,
+				playing_cards_created = { true }
+			}
 		end
 	end
 }
